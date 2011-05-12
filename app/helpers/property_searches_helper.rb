cc67507f5ced39cdae546e5b20722bd3796a1a0a
jason gagne <jgagne33@gmail.com>
2011-05-12 07:03:02 -0700

    adding helpers

--- app/helpers/property_searches_helper.rb
+++ app/helpers/property_searches_helper.rb
@@ -2,7 +2,7 @@ module PropertySearchesHelper
   def display_status(property)
     unless property.status == "Active" || property.status.blank?
       "<span class=\"#{property.status.downcase}\">#{property.status}</span>"
-    selse
+    else
       ""
     end
   end
