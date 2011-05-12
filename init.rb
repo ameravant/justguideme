11b42015e75a81823b207daa2c2906f7da4ce2c3
jason gagne <jgagne33@gmail.com>
2011-05-12 06:58:40 -0700

    adding helpers

--- init.rb
+++ init.rb
@@ -4,4 +4,7 @@ require 'events_ext'
 require "12_hour_time"
 config.to_prepare do
   ApplicationController.helper(PropertiesHelper)
+  ApplicationController.helper(ProfilesHelper)
+  ApplicationController.helper(PropertySearchesHelper)
+  ApplicationController.helper(RegionsHelper)
 end
