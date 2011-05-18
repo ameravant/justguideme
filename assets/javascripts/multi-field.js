2d4b04e623b1b4fcdca4296fd35ddd187695cc3e
jason gagne <jgagne33@gmail.com>
2011-05-18 09:39:42 -0700

    adding image button

--- assets/javascripts/multi-field.js
+++ assets/javascripts/multi-field.js
@@ -1,7 +1,7 @@
 function initMultiField() {
 	var num = parseInt(jQuery(".add-images input:last").attr("id").replace(/^\D+/i, "").replace(/_\D+$/i, "")) + 1;
 	var obj = jQuery(".add-images input:last").attr("id").replace(/_images.*$/i, "");
-	var address = jQuery("span.hidden-for-js").val();
+	var address = jQuery("span.hidden-for-js").text();
 	jQuery(".add-new-image").click(function() {
 		var new_input = "<div class=\"img-form\" style=\"margin-left:7.3em;\"><span><label for=\"" + obj + "_images_attributes_" + num + "_title\">Title</label> <input id=\"" + obj + "_images_attributes_" + num + "_title\" name=\"" + obj + "[images_attributes][" + num + "][title]\" size=\"30\" type=\"text\" value=\"" + address + "\" /> <label for=\"" + obj + "_images_attributes_" + num + "_title\">Image</label><input id=\"" + obj + "_images_attributes_" + num + "_image\" name=\"" + obj + "[images_attributes][" + num + "][image]\" size=\"30\" type=\"file\" /></span></div>"
 		jQuery(".add-images").prepend(new_input);
